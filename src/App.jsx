70b769c5c803fb44b9ab009065d8c5a4727ad0a4
Abhay Murali M <abhaymuralim9@gmail.com>
2026-01-11 10:28:34 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div className='text-3xl text-red-500'>
-      Hello World!
-    </div>
-  )
+    <>
+      <section className="">
+        <div className="">
+          <div className="">
+            <h1 className="Find the right course for you"></h1>
+            <h2 className="">
+              See your personalized recommendations based on your interest and
+              goals
+            </h2>
+            <div className="">
+              <button className=""></button>
+              <button className=""></button>
+            </div>
+          </div>
+          <div className="">
+            
+          </div>
+        </div>
+        <div className=""></div>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
